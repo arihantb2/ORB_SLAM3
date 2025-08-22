52da5a365b381c688409d82a801e40a8f8acd546
tberriel <57666961+tberriel@users.noreply.github.com>
2025-08-22 14:13:45 +0200

    Adapted to avoid unnecesary builds

--- build.sh
+++ build.sh
@@ -1,40 +1,41 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
-
-cd Thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-
-cd ../../g2o
+#echo "Configuring and building Thirdparty/DBoW2 ..."
+#cd Thirdparty/DBoW2
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j
+#cd ../..
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+cd g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
+cd ../../
 
-cd ../../Sophus
 
-echo "Configuring and building Thirdparty/Sophus ..."
+#echo "Configuring and building Thirdparty/Sophus ..."
+#cd Sophus
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j
+#cd ../../
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-
-cd ../../../
-
+cd ../
 echo "Uncompress vocabulary ..."
 
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ..
+if [ ! -f Vocabulary/ORBvoc.txt ]; then
+    cd Vocabulary
+    tar -xf ORBvoc.txt.tar.gz
+    cd ..
+fi
 
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
 make -j4
